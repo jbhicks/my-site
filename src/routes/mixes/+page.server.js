d558d28909b8c429dfd80679e149137c242f2e7b
Joshua Hicks <joshua.brock.hicks@gmail.com>
2023-09-06 01:46:30 -0700

    loading soundcloud feed

--- src/routes/mixes/+page.server.js
+++ src/routes/mixes/+page.server.js
@@ -1,7 +1,5 @@
-import { json } from '@sveltejs/kit';
-
-/** @type {import('./$types').RequestHandler} */
-export async function GET(e) {
+/** @type {import('./$types').PageServerLoad} */
+export async function load() {
     const data = await fetch('https://us-central1-my-web-site-27a15.cloudfunctions.net/getSCStream?offset=0&limit=100');
     const json = await data.json();
     const response = new Response(JSON.stringify(json), {
@@ -9,6 +7,6 @@ export async function GET(e) {
             'content-type': 'application/json'
         }
     });
-    console.log(response);
-    return response;
+    return response.json();
+
 };
--- src/routes/mixes/+page.svelte
+++ src/routes/mixes/+page.svelte
@@ -1,3 +1,19 @@
-<h1 class="text-3xl font-bold underline">Latest Mixes</h1>
+<script>
+	export let data;
+</script>
 
-<p>Currently under construction.</p>
+{#each data.collection as mix}
+	<div class="collapse bg-base-200">
+		<input type="radio" name="my-accordion-1" checked="checked" />
+		<div class="collapse-title text-xl font-medium">
+			<h2 class="card-title">{mix?.track?.title}</h2>
+		</div>
+		<div class="collapse-content flex items-center">
+			<figure>
+				<img src={mix?.track?.artwork_url} class="rounded-t-lg" />
+			</figure>
+			<p class="p1-4">{mix?.track?.description}</p>
+		</div>
+	</div>
+	<div class="divider" />
+{/each}
