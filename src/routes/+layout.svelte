9ddf5cf58f7264651be37391b94e812afce0f2e2
Joshua Hicks <joshua.brock.hicks@gmail.com>
2023-09-08 08:53:49 -0700

    static build successful

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 <script>
 	import '$lib/styles/global.css';
 	import Header from '$lib/components/Header.svelte';
+  export const prerender = true;
 </script>
 
 <Header></Header>
--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { mdsvex } from 'mdsvex';
 import sveltePreprocess from 'svelte-preprocess';
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	base: '/my-app',
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH,
+		},
 	},
 	extensions: ['.svelte', '.md'],
 	preprocess: [
 		sveltePreprocess(),
 		mdsvex({
 			extensions: ['.md']
-		})	
+		})
 	],
 };
 
