cbbf89eb12d25a549b90d4f29b37cacffa047037
Josh Hicks <joshua.brock.hicks@gmail.com>
2023-09-07 19:47:16 -0700

    modified the layout a bit

--- src/routes/mixes/+page.svelte
+++ src/routes/mixes/+page.svelte
@@ -5,13 +5,18 @@
 {#each data.collection as mix}
 	<div class="collapse bg-base-200">
 		<input type="radio" name="my-accordion-1" checked="checked" />
+
 		<div class="collapse-title text-xl font-medium">
-			<h2 class="card-title">{mix?.track?.title}</h2>
+			<figure class="flex items-center">
+				<img src={mix?.track?.artwork_url} class="rounded-t-lg" alt={mix?.track?.title} />
+				<figcaption class="p-4">
+					<div class="text-xl font-medium">{mix?.track?.title}</div>
+					<div class="text-gray-500">{mix?.track?.user?.username}</div>
+					<audio src={mix?.track?.stream_url} controls />
+				</figcaption>
+			</figure>
 		</div>
 		<div class="collapse-content flex items-center">
-			<figure>
-				<img src={mix?.track?.artwork_url} class="rounded-t-lg" />
-			</figure>
 			<p class="p1-4">{mix?.track?.description}</p>
 		</div>
 	</div>
